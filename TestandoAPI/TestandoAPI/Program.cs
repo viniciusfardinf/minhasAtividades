7efa1dea18002261b9b589589abe2a7e30bba870
Vinicius <Vinicius@tectrilha.local>
2026-04-22 15:02:19 -0300

    testando HTTP e try-catch

---
+++ TestandoAPI/TestandoAPI/Program.cs
@@ -0,0 +1,29 @@
+﻿    try
+    {
+    Console.WriteLine("Informe o primeiro número: ");
+    double a = double.Parse(Console.ReadLine()!);
+
+    Console.WriteLine("Informe o segundo número: ");
+    double b = double.Parse(Console.ReadLine()!);
+
+    if (b == 0)
+        {
+            Console.WriteLine("Não é possível dividir um número por 0");
+        }
+        else
+        {
+            double divisao = a / b;
+            Console.WriteLine($"A divisão é {divisao}");
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Entrada inválida! Digite apenas números.");
+    }
+
+
+
+
+
+
+
